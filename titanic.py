88a5494ad0f14fb04ccc9ec7f33e93e9150c1806
Anubhav Thapa <magaranub@gmail.com>
2020-08-05 23:44:34 +0545

    XG boost

--- titanic.py
+++ titanic.py
@@ -15,6 +15,8 @@ from sklearn.model_selection import GridSearchCV
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.ensemble import AdaBoostClassifier
 from sklearn.ensemble import RandomForestClassifier
+from xgboost import XGBClassifier
+
 
 train_csv=pd.read_csv('test.csv')
 df=pd.read_csv('train.csv')
@@ -202,3 +204,47 @@ rggrid.fit(dc_Xtrain,dc_ytrain)
 ygridpred=rggrid.best_estimator_
 new_y=ygridpred.predict(dc_Xtest)
 print(accuracy_score(dc_ytest,new_y))
+
+xgbm=XGBClassifier( )
+xgbm.fit(X_train,y_train)
+xgb_pred=xgbm.predict(X_test)
+print(accuracy_score(y_test,xgb_pred))
+
+rand_params={'learning_rate':[0.1,0.01,0.001,0.001],'max_depth':[1,3,5,7,10],'n_estimators':[50,100,200,300,500],
+            "gamma" : [0.01, 0.1, 0.3, 0.5, 1, 1.5, 2],
+               "colsample_bytree": [0.3, 0.6, 0.8, 1.0],
+               "subsample": [0.2, 0.4, 0.5, 0.6, 0.7],
+               "reg_alpha": [0, 0.5, 1],
+               "reg_lambda": [1, 1.5, 2, 3, 4.5],
+               "min_child_weight": [1, 3, 5, 7]}
+cgb_rand=RandomizedSearchCV(xgbm,rand_params,cv=3,n_jobs=-1,random_state=40)
+cgb_rand.fit(X_train,y_train)
+print(cgb_rand.best_params_   )            
+grid_params={'learning_rate':[cgb_rand.best_params_['learning_rate'],cgb_rand.best_params_['learning_rate']+0.001,cgb_rand.best_params_['learning_rate']+0.03],
+             'max_depth':[cgb_rand.best_params_['max_depth']-1,cgb_rand.best_params_['max_depth'],cgb_rand.best_params_['max_depth']+1,cgb_rand.best_params_['max_depth']+2],
+             'n_estimators':[cgb_rand.best_params_['n_estimators']-20,cgb_rand.best_params_['n_estimators'],cgb_rand.best_params_['n_estimators']+20,cgb_rand.best_params_['n_estimators']+30,cgb_rand.best_params_['n_estimators']+100],
+            "gamma" : [0,cgb_rand.best_params_['gamma']+0.1,cgb_rand.best_params_['gamma']+0.001,cgb_rand.best_params_['gamma']+0.2,cgb_rand.best_params_['gamma']+0.3,cgb_rand.best_params_['gamma']+0.4],
+               "colsample_bytree": [cgb_rand.best_params_['colsample_bytree']-0.5,cgb_rand.best_params_['colsample_bytree']-0.3,cgb_rand.best_params_['colsample_bytree'],cgb_rand.best_params_['colsample_bytree']+0.1,cgb_rand.best_params_['colsample_bytree']+0.001],
+               "subsample": [cgb_rand.best_params_['subsample']-0.2,cgb_rand.best_params_['subsample']-0.3,cgb_rand.best_params_['subsample'],cgb_rand.best_params_['subsample']+0.1,cgb_rand.best_params_['subsample']+0.001],
+               "reg_alpha": [cgb_rand.best_params_['reg_alpha']+0.3,cgb_rand.best_params_['reg_alpha']+0.4,cgb_rand.best_params_['reg_alpha'],cgb_rand.best_params_['reg_alpha']+0.8],
+               "reg_lambda": [0,cgb_rand.best_params_['reg_lambda']+1,cgb_rand.best_params_['reg_lambda']+0.7,cgb_rand.best_params_['reg_lambda'],cgb_rand.best_params_['reg_lambda']+4.5,cgb_rand.best_params_['reg_lambda']+2.5],
+               "min_child_weight": [cgb_rand.best_params_['min_child_weight']+30,cgb_rand.best_params_['min_child_weight']+5,cgb_rand.best_params_['min_child_weight'],cgb_rand.best_params_['min_child_weight']+10,cgb_rand.best_params_['min_child_weight']+20]}
+grid_cgb=GridSearchCV(xgbm,grid_params,cv=10,n_jobs=1)
+
+
+new_tr=pd.get_dummies(train_csv['Sex'],drop_first=True)
+filt=train_csv.drop(['Parch','SibSp','PassengerId','Cabin','Name','Sex','Embarked','Ticket'],axis=1)
+train_x=pd.concat([filt,new_tr],axis=1)
+train_x['Fare'].fillna(train_x['Fare'].mean(),inplace=True)
+train_x['Age'].fillna(train_x['Age'].mean(),inplace=True)
+tr_X=train_x.iloc[:,:]
+tr_X_sc=sc.fit_transform(tr_X)
+y_xgb_pred=xgbm.predict(tr_X_sc).astype(int)
+tr_X['Survived']=pd.DataFrame({'Survived':y_xgb_pred})
+new_dat=pd.concat([train_csv['PassengerId'],tr_X['Survived']],axis=1)
+new_dat.to_csv('submission.csv',index=False)
+submitt_csv=pd.read_csv('submission.csv')
+
+
+
+
